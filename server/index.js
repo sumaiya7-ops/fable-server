8ad3dfad4bfe03e1544141857f3a0f5080a00c66
sumaiya7-ops <sumaiya7-ops@gmail.com>
2026-06-28 21:36:31 +0600

    add storage

--- server/index.js
+++ server/index.js
@@ -16,6 +16,14 @@ if (!fs.existsSync(uploadDir)) {
 }
 
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, uploadDir);
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
 
 
 const upload = multer({ storage: storage });
